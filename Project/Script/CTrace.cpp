68f0bd8fb904f0f18955af4ebd0b53d460f60ee6
현구 강 <rgr2718@naver.com>
2023-07-17 05:18:41 +0900

    Monster Script에 Trace 추가

--- Project/Script/CTrace.cpp
+++ Project/Script/CTrace.cpp
@@ -18,29 +18,30 @@ void CTrace::tick()
 	{
 		m_fLastRenewal -= m_fRenewal_Trace;
 		memcpy(m_vActualPath, CDetourMgr::GetInst()->GetPathtoTarget(GetOwner()->Transform()->GetWorldPos()), sizeof(Vec3) *256);
-
+		m_icurrentPathIndex = 0;
 	}
 
-	if (currentPathIndex < m_iactualPathCount)
+	if (m_icurrentPathIndex < m_iActualPathCount)
 	{
 		// 다음 노드(메시) 위치
-		Vec3 targetPos(actualPath[3 * currentPathIndex], actualPath[3 * currentPathIndex + 1], actualPath[3 * currentPathIndex + 2]);
+		Vec3 targetPos = m_vActualPath[m_icurrentPathIndex];
 
 		// 현재 오브젝트 위치		
-		Vec3 currentPos = Vec3(_vStartPos.x, _vStartPos.y, -_vStartPos.z);
+		Vec3 currentPos = GetOwner()->Transform()->GetWorldPos();
+		currentPos.z *= -1.f;
 
 		// 이동할 방향 벡터 계산 및 정규화
 		Vec3 direction = targetPos - currentPos;
 		direction = direction.Normalize();
 
 		// 새로운 위치 계산
-		Vec3 newPos = currentPos + speed * direction * DT;
+		Vec3 newPos = currentPos + m_fSpeed * direction * DT;
 
 		// 만약 타겟 위치에 도달했다면, 다음 경로 인덱스.
 		float distanceToTarget = (targetPos - newPos).Length();
-		if (distanceToTarget < speed * DT)
+		if (distanceToTarget < m_fSpeed * DT)
 		{
-			currentPathIndex++;
+			m_icurrentPathIndex++;
 		}
 	}
 }
--- Project/Script/CTrace.h
+++ Project/Script/CTrace.h
@@ -6,9 +6,9 @@ class CTrace :
 private:
     float m_fLastRenewal;
     float m_fRenewal_Trace; // 추적 경로를 갱신하는 주기
-
+    float m_fSpeed;
     Vec3  m_vActualPath[256];
-    int   m_iactualPathCount;
+    int   m_iActualPathCount;
     int   m_icurrentPathIndex;
 
 public:
