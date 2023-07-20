2a272fb71af6d144d72ae99f5f9f2ee6a44f93ac
l980l <k910208@gmail.com>
2023-07-20 17:48:49 +0900

    MonsterDetectRangeScript도 cpp 올리기

---
+++ Project/Script/CMonsterDetectRangeScript.cpp
@@ -0,0 +1,32 @@
+#include "pch.h"
+#include "CMonsterDetectRangeScript.h"
+#include "CMonsterScript.h"
+
+void CMonsterDetectRangeScript::begin()
+{
+}
+
+void CMonsterDetectRangeScript::tick()
+{
+}
+
+void CMonsterDetectRangeScript::BeginOverlap(CCollider3D* _Other)
+{
+	// Player¿Í ºÎµúÈù °æ¿ì¿¡¸¸.
+	if (_Other->GetOwner() == CLevelMgr::GetInst()->FindObjectByName(L"Player"))
+	{
+		m_bDetect = true;
+		
+		GetOwner()->GetParent()->GetScript<CMonsterScript>()->SetDetect(true);
+	}
+}
+
+CMonsterDetectRangeScript::CMonsterDetectRangeScript()
+	: CScript((UINT)SCRIPT_TYPE::MONSTERDETECTRANGESCRIPT)
+	, m_bDetect(false)
+{
+}
+
+CMonsterDetectRangeScript::~CMonsterDetectRangeScript()
+{
+}
